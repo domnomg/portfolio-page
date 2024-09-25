322ea785e8bd453bbaaa2b1c45235705b7e96b3b
Dominique Gavin <gado@bluewin.ch>
2024-09-25 16:41:06 +0200

    Initialer Commit

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// Wenn der Button geklickt wird, ändere den Text im Absatz
+document.getElementById('klickButton').addEventListener('click', function() {
+    document.getElementById('outputText').innerText = "Du hast den Button geklickt!";
+});
+
